8d5ded3bed02701e513da5adbb3e3ea596e8e478
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-17 17:55:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dodger.style.bottom = "100px"
 
 dodger.style.bottom = "0px"
 
-moveDodgerLeft() =>  {
+function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
