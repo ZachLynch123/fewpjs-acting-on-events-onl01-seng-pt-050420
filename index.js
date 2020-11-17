d8448414080d93dc04b2057cacaf0ac647c8b1b0
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-17 17:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ dodger.style.bottom = "100px"
 
 dodger.style.bottom = "0px"
 
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
