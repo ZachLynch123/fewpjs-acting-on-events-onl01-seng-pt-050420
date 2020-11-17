21c5acfe1e27991fb58efda1b9fe4971703588be
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-17 17:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNUmbers, 10);
   
   if (right > 0) {
-    dodger.style.right = `${right - 1 }px`
+    dodger.style.right = `${right - 1 }px`;
   }
 }
 
